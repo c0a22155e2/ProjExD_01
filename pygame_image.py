98c5b47d36a1164bc4c1602a40dbcaf0f8a04a8a
c0a22155e2 <c0a22155e2@edu.teu.ac.jp>
2023-09-26 18:13:59 +0900

    演習課題終了

--- pygame_image.py
+++ pygame_image.py
@@ -7,15 +7,27 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True ,False)
+    tori = pg.image.load("ex01/fig/3.png")
+    tori1 = pg.transform.flip(tori, True ,False)
+    tori2 = pg.transform.rotozoom(tori1, 10,1.0)
+    t_list = [tori1 , tori2]
     tmr = 0
+    x = tmr
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])
+        
+        screen.blit(bg_img, [0-x, 0])
+        screen.blit(bg_img2, [1600-x,0])
+        screen.blit(bg_img, [3200-x, 0])
+        screen.blit(t_list[(x//50) %2] , [300,200])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(100)
+        x += 1
+        if x > 3200:
+            x = 0
 
 
 if __name__ == "__main__":
@@ -23,3 +35,5 @@ if __name__ == "__main__":
     main()
     pg.quit()
     sys.exit()
+
+
